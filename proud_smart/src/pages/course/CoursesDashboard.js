2b7122e195d0eeaa4bef9ddd5df6f784b775a50e
Ari <afriedgut1@gmail.com>
2019-07-13 13:43:41 +1000

    completed course dashboard component and chapter component skeleton

--- proud_smart/src/pages/course/CoursesDashboard.js
+++ proud_smart/src/pages/course/CoursesDashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
-import { Layout } from "antd";
+import { Layout, Divider } from "antd";
+import Chapters from "./Chapters";
 
 class CoursesDashboard extends Component {
   componentDidMount() {
@@ -10,7 +11,7 @@ class CoursesDashboard extends Component {
   render() {
     const { Sider, Content } = Layout;
     return (
-      <Layout>
+      <Layout style={{ minHeight: "100vh" }}>
         <Content>
           <div className="video-container">
             <iframe
@@ -21,9 +22,53 @@ class CoursesDashboard extends Component {
               allowfullscreen
             />
           </div>
-          <h3>About This Course</h3>
+          <div className="section">
+            <div className="row">
+              <div className="col s6 left-align">
+                <h3>About This Course</h3>
+                <h4>This is a good course</h4>
+              </div>
+              <div className="col s6 right-align">
+                <h5>Course Content</h5>
+              </div>
+            </div>
+            <Divider />
+            <div className="row">
+              <h5>Heading</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                quos pariatur saepe odit ad reiciendis deleniti doloribus. Alias
+                natus unde nisi non et porro, beatae ad, tempore magnam optio
+                dolor.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                quos pariatur saepe odit ad reiciendis deleniti doloribus. Alias
+                natus unde nisi non et porro, beatae ad, tempore magnam optio
+                dolor.
+              </p>
+            </div>
+            <Divider />
+            <div className="row">
+              <h5>The Teacher</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                quos pariatur saepe odit ad reiciendis deleniti doloribus. Alias
+                natus unde nisi non et porro, beatae ad, tempore magnam optio
+                dolor.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                quos pariatur saepe odit ad reiciendis deleniti doloribus. Alias
+                natus unde nisi non et porro, beatae ad, tempore magnam optio
+                dolor.
+              </p>
+            </div>
+          </div>
         </Content>
-        <Sider />
+        <Sider>
+          <Chapters />
+        </Sider>
       </Layout>
     );
   }
