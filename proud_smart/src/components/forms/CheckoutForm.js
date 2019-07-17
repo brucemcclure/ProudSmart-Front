7e1f32d2c7561f8beba02c46559565737a051927
Billy Leitch <wlei6277@gmail.com>
2019-07-17 19:27:49 +1000

    Added metadata to the stripe request. This will enable the database to be updated by the express server

--- proud_smart/src/components/forms/CheckoutForm.js
+++ proud_smart/src/components/forms/CheckoutForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
-import Axios from "axios";
+import LocalAPI from "./../../apis/Local";
+import {connect} from "react-redux";
 
 class CheckoutForm extends Component {
   constructor(props) {
@@ -11,10 +12,29 @@ class CheckoutForm extends Component {
 
   async submit(ev) {
     // User clicked submit
-    let { token } = await this.props.stripe.createToken({ name: "Name" });
-    let response = await Axios.post("http://localhost:3002/payments/charge", {
+    let { token } = await this.props.stripe.createToken({ 
+      name: "name",
+
+      
+     });
+    console.log(token);
+    let response = await LocalAPI.post("/payments/charge", {
       //   headers: { "Content-Type": "text/plain" },
-      token: token.id
+      token: token,
+
+      // the metadata is used to adjust the database with the payment information
+      // the values below have been hardcoded but will need to be updated with the values of course being purchased 
+      metadata: {
+        amount: 20000, // insert the amount to charge the customer REMEMBER THIS IS IN CENTS!!!!!
+        userId: "ankjfdkj", // insert the id of the user who purchased the course
+        purchasedCourse: {
+          courseId: "abcd", // insert the id of the course they purchased
+          courseTitle: "spanking", // insert the title of the course
+          courseProfileUrl: "www.goodtimes.com", // insert the profile photo url
+          description: "don't be naughty" // insert the course description
+        }
+
+      }
     });
     console.log(response);
     if (response.status === 200) this.setState({ complete: true });
