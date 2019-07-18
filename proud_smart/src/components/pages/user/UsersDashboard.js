629ed721b3f75775e53e31a10a551e08fb7407f5
Ari <afriedgut1@gmail.com>
2019-07-18 16:14:31 +1000

    removed token deconstruction from users dashboard

--- proud_smart/src/components/pages/user/UsersDashboard.js
+++ proud_smart/src/components/pages/user/UsersDashboard.js
@@ -11,15 +11,9 @@ const squareCardHolder = {
 
 class UsersDashboard extends Component {
   render() {
-    const { token } = this.props;
     return (
-<<<<<<< HEAD
-      <>
-        <h1>This is the UsersDashboard</h1>
-=======
       <div className="container section">
         <h1>This is the UsersDashboard</h1>;
->>>>>>> a13a6ca590b9d0351e95b1662db3fc8e09fc564a
         <Link to="/courses/show" className="btn">
           Become a Teacher
         </Link>
