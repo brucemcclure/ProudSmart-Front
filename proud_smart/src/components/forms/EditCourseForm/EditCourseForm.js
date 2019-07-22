2b1f84e2a5947d79028d372e959871fdc92e5719
Bruce Mc Clure <brucemcc90@gmail.com>
2019-07-22 14:16:32 +1000

    fixed edit course form

--- proud_smart/src/components/forms/EditCourseForm/EditCourseForm.js
+++ proud_smart/src/components/forms/EditCourseForm/EditCourseForm.js
@@ -37,7 +37,10 @@ class EditCourseForm extends Component {
     values.courseProfilePictureUrl = "www.PleaseAddThisFeatureJoshOrBruce";
     console.log(values);
 
-    LocalAPI.post("courses", values)
+    LocalAPI.put(`/courses/${this.props.course._id}`, {
+      course: this.props.course,
+      values
+    })
       .then(data => console.log(data))
       .catch(err => console.log(err));
   };
